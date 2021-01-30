f141b94aa3d9dfea6af2a093e25b873939c08788
salvador984 <salvador.ant@gmail.com>
2021-01-30 13:07:50 +0100

    Arreglado errores

--- Ej17/main.js
+++ Ej17/main.js
@@ -1,21 +1,4 @@
-class Cliente {
-    constructor(nombre, direccion, telefono, nif) {
-      this.nombre = nombre;
-      this.direccion = direccion;
-      this.telefono = telefono;
-      this.nif = nif;
-    }
-  }
-  
-  class Elemento {
-    constructor(descripcion, cantidad, precio) {
-      this.descripcion = descripcion;
-      this.cantidad = cantidad;
-      this.precio = precio;
-    }
-  }
-  
-  class Factura {
+class Factura {
     constructor(cliente, elementos) {
       this.cliente = cliente;
       this.elementos = elementos;
@@ -24,23 +7,40 @@ class Cliente {
         iva: 21,
         total:0,
         formaPago: "contado"
-      };
-  
-      this.calculaTotal = () => {
-        for (var i = 0; i <this.elementos.length; i++) {
-          this.informacion.baseImponible += this.elementos[i].cantidad * this.elementos[i].precio;
-        }
-        this.informacion.total = this.informacion.baseImponible * (1+(this.informacion.iva/100));
       }
-  
+
+      this.calculoTotal =function() {
+         for (var i=0; i< this.elementos.length; i++){
+             this.informacion.baseImponible += this.elementos[i].cantidad * this.elementos[i].precio; 
+         } 
+         this.informacion.total = this.informacion.baseImponible * (1+(this.informacion.iva/100));
+      }
+
       this.muestraTotal = function () {
-        this.calculaTotal();
-        alert("total = " + this.informacion.total + " euros");
-      }
-    };
-  }
-  
-  var elCliente = new Cliente("Cliente 1", "", "", "");
-  var losElementos = [new Elemento("elemento1", "1", "5"), new Elemento("elemento2", "2", "12")];
-  var laFactura = new Factura(elCliente, losElementos);
-  laFactura.muestraTotal();
+        this.calculoTotal();
+        alert("El total de los productos es = " + this.informacion.total + " euros");
+        }
+    }
+}
+
+class Cliente {
+    constructor(nombre, direccion, telefono, nif) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.nif = nif;
+    }
+}
+      
+class Elemento {
+    constructor(descripcion, cantidad, precio) {
+        this.descripcion = descripcion;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+}
+
+const primerCliente = new Cliente("Cliente 1. Juana Mari", "Madrid", "911", "123456789");
+const arrayElementos = [new Elemento("elemento1", 1, 5), new Elemento("elemento2", 2, 12)];
+const facturaDelTotal = new Factura(primerCliente, arrayElementos);
+facturaDelTotal.muestraTotal();
